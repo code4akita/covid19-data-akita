0201c4e5b5cb1f41d57aa97ede33fad7000440c0
Katsuyoshi Ito <kito@itosoft.com>
2020-11-30 17:29:49 +0900

    11/29更新のHTML構造に対応
    ・感染者の概要を最初のテーブルから引っ張っていたが、captionで判断する様に変更
    ・感染者の概要が現在の感染者の内容を別テーブルに移しているのに対応。項目も増えている。(現在の感染者数 自宅療養者数 入院等調整中)
    ・検査実施件数の推移が2ページに分離されたのに対応。
    ・検査実施件数の推移も2番目のテーブルから引っ張っていたが、captionで判断する様にした。
    ・また検査実施件数推移の過去データに県（健康環境センター）と秋田市（保健所）のみのデータが追加され、前と同じになる様に秋田県総合保健事業団を含んだデータの方。(後の方)を使う様にしている。今後順番が変わったら対応が必要。

--- lib/mkjson.rb
+++ lib/mkjson.rb
@@ -32,28 +32,32 @@ def mkjson
 
   urls.each do |url|
     doc = Nokogiri::HTML(URI.open(url))
-    doc.search(".c-table--full")[0].inner_text.each_line do |l|
-      l = l.chomp!
-      case l
-      when "", "<hr>"
-        next
-      when /^\d+例目/
-        info['感染者の概要']['context'] << {}
-        index = 0
-      end
-      next unless index
+    doc.search(".c-table--full").each do |table|
+      if table.search("caption").inner_text.include?("概要")
+        table.inner_text.each_line do |l|
+          l = l.chomp!
+          case l
+          when "", "<hr>"
+            next
+          when /^\d+例目/
+            info['感染者の概要']['context'] << {}
+            index = 0
+          end
+          next unless index
 
-      key = keys[index]
-      next unless key
+          key = keys[index]
+          next unless key
 
-      e = info['感染者の概要']['context'].last
-      case key
-      when '県内症例'
-        e[key] = l.to_i
-      else
-        e[key] = l
+          e = info['感染者の概要']['context'].last
+          case key
+          when '県内症例'
+            e[key] = l.to_i
+          else
+            e[key] = l
+          end
+          index += 1
+        end
       end
-      index += 1
     end
   end
   info['感染者の概要']['context'].sort!{|a,b| a['県内症例'] <=> b['県内症例']}
@@ -72,21 +76,34 @@ def mkjson
     'updated_at' => now,
   }
 
-  doc = Nokogiri::HTML(URI.open(url))
-
   index = nil
-  keys = %w(感染者数累計 入院者数 うち重症者数 宿泊療養者数 退院者・療養解除者数 死亡者数)
-  doc.inner_text.each_line do |l|
-    case l
-    when /死亡者数/
-      index = 0
-    when /(\d+)人/
-      if index
-        key = keys[index]
-        if key
-          info['現在の入退院者数等']['context'][key] = $1.to_i
-          index += 1
+  keys = %w(感染者数累計 入院者数 うち重症者数 宿泊療養者数 退院者・療養解除者数 死亡者数 現在の感染者数 自宅療養者数 入院等調整中)
+
+  doc = Nokogiri::HTML(URI.open(url))
+  doc.search(".c-table--full").reverse.each do |table|
+    case table.search("caption").inner_text
+    when /県内感染者の状況/, /現在の感染者の内訳/
+
+      trs = table.search("tr")
+      headers = trs[0].search("th").map{|td| td.inner_text.chomp }
+      values = trs[1].search("td").map{|td| td.inner_text.chomp }
+
+      headers.each_with_index do |title, i|
+        
+        # 必要があればキーの置換
+        case title
+        when /入院者数のうち重症者数/
+          title = 'うち重症者数'
+        when /計/
+          title = '感染者数累計'
         end
+
+        # 人数を記録
+        if keys.include?(title)
+          v = values[i].scan(/(\d+)人/).first.first.to_i
+          info['現在の入退院者数等']['context'][title] = v
+        end
+
       end
     end
   end
@@ -94,39 +111,55 @@ def mkjson
   #==========================================
   # 検査実施件数の推移を取得
 
+  urls = %w(
+    https://www.pref.akita.lg.jp/pages/archive/51592
+    https://www.pref.akita.lg.jp/pages/archive/54031
+  )
+
   info['検査実施件数の推移'] = {
-    'url' => url,
+    'url' => urls.last,
+    'urls' => urls,
     'context' => [],
     'updated_at' => now,
   }
-
+  
   index = nil
   keys = %w(期間 PCR検査実施件数 うち陽性件数)
-  doc.search(".c-table--full")[1].inner_text.each_line do |l|
-    l.chomp!
-    case l
-    when ""
-    when /うち陽性件数/
-      index = 0
-    when /合計/
-      index = nil
-    else
-      if index
-        key = keys[index]
-        if key
-          info['検査実施件数の推移']['context'] << {} if index == 0
-          case key
-          when "PCR検査実施件数", "うち陽性件数"
-            info['検査実施件数の推移']['context'].last[key] = l.to_i
+
+  urls.each do |url|
+    doc = Nokogiri::HTML(URI.open(url))
+    doc.search(".c-table--full").reverse.each do |div|
+      if div.inner_text.include?("検査実施件数の推移")
+        div.inner_text.each_line do |l|
+          l.chomp!
+          case l
+          when ""
+          when /内訳はこちら/ # 別ページに詳細があるので含めない
+            index = nil
+          when /うち陽性件数/
+            index = 0
+          when /合計/
+            index = nil
           else
-            info['検査実施件数の推移']['context'].last[key] = l
+            if index
+              key = keys[index]
+              if key
+                info['検査実施件数の推移']['context'] << {} if index == 0
+                case key
+                when "PCR検査実施件数", "うち陽性件数"
+                  info['検査実施件数の推移']['context'].last[key] = l.gsub(/,/, '').to_i  # gsubは数値がカンマ区切りなので
+                else
+                  info['検査実施件数の推移']['context'].last[key] = l
+                end
+                index = (index + 1) % 3
+              end
+            end
           end
-          index = (index + 1) % 3
         end
+        break
       end
     end
   end
-
 
   #==========================================
   # S3にアップロード
